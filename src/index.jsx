8312fcd9a05ff2218219ebb25cb45c2ae3766e85
MahyaBaghai <mahyabaghai@gmail.com>
2025-06-12 10:03:16 +0330

    add deploy

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter  basename="/CoffeeShop">
     <I18nextProvider i18n={i18n}>
       <App />
     </I18nextProvider>
